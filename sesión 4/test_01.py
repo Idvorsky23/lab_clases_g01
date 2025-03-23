fd72733680e5cce28349cd942d2d336591358027
Cesar Gutierrez <idvorsky23@gmail.comgit config --global user.name Cesar Gutierrez>
2025-03-23 13:27:30 -0500

    first files

---
+++ sesión 4/test_01.py
@@ -0,0 +1,13 @@
+"""Diccionario en Python"""
+"""Los nombres de las key(llaves) van a ir escritos siempre en inúscula y
+sin tildes (por convección o por buenas práctica"""
+var_1 = {"nombre": "Susana", "edad":24, "promedio": 14.9}
+print("Mi diccionario tiene el siguiente contenido:",var_1)
+
+"""Agreggamos elementos con un nuevo key a mi diccionario"""
+
+var_1["distrito"] = "Villa El Salvador"
+var_1["nombre"] = "Cesar"
+var_1["habilitado"] = True
+
+print(var_1)
---
+++ sesión 4/test_03.py
@@ -0,0 +1,9 @@
+"""Diccionario en Python"""
+"""sorted: obtener los valores de los key de un diccionario"""
+
+var_1 = {"nombre":"MYSQL", "Tipo": "BD relacional"}
+
+keys = sorted(var_1)
+
+print(var_1)
+print(keys)
